6ab3f383fc8ec35ad49ecf69a3f973a2c2c8b5d0
Jimbo77 <chupyalcohol@gmail.com>
2017-02-27 11:20:35 -0600

    Revert "sweep2sleep: fix device name"
    This reverts commit 35ca0a0547ed62c6586eb5a8ca930384092bee6b.

--- drivers/input/touchscreen/sweep2sleep.c
+++ drivers/input/touchscreen/sweep2sleep.c
@@ -186,7 +186,7 @@ static void s2s_input_event(struct input_handle *handle, unsigned int type,
 }
 
 static int input_dev_filter(struct input_dev *dev) {
-	if (strstr(dev->name, "synaptics")) {
+	if (strstr(dev->name, "synaptics,s3320")) {
 		return 0;
 	} else {
 		return 1;
