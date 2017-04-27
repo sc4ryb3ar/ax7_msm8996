4df26541fb24b15976ee484bfba3babbed2d01ec
Jimbo77 <chupyalcohol@gmail.com>
2017-04-27 10:18:38 -0600

    Revert "state_notifier: set DEFAULT_SUSPEND_DEFER_TIME to 1."
    This reverts commit eacd9c095299fc3923138ea42e10f41636a531c4.

--- drivers/soc/qcom/state_notifier.c
+++ drivers/soc/qcom/state_notifier.c
@@ -14,7 +14,7 @@
 #include <linux/export.h>
 #include <linux/module.h>
 
-#define DEFAULT_SUSPEND_DEFER_TIME 	1
+#define DEFAULT_SUSPEND_DEFER_TIME 	10
 #define STATE_NOTIFIER			"state_notifier"
 
 /*
